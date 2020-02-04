c8cf5be34ad783d24958eaee4ecc9ad12d32df93
GalaxyGorilla <sascha@netdef.org>
2020-02-04 11:42:55 +0000

    Introduce ZAPI_MESSAGE_SRTE

--- bgpd/bgp_zebra.c
+++ bgpd/bgp_zebra.c
@@ -1265,6 +1265,11 @@ void bgp_zebra_announce(struct bgp_dest *dest, const struct prefix *p,
 		api.tableid = info->attr->rmap_table_id;
 	}
 
+	if (CHECK_FLAG(info->attr->flag, BGP_ATTR_SRTE_COLOR)
+	    || CHECK_FLAG(info->attr->flag, BGP_ATTR_SRTE_POLICY))
+
+		SET_FLAG(api.message, ZAPI_MESSAGE_SRTE);
+
 	/* Metric is currently based on the best-path only */
 	metric = info->attr->med;
 
