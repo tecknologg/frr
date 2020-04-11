c49df3104cdea39614c2eb7e3499b21c8e3e910e
Renato Westphal <renato@opensourcerouting.org>
2020-04-11 00:09:05 -0300

    lib: fix nexthop comparison to take SR-TE colors into account
    Fixes #62.
    
    Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

--- lib/nexthop.c
+++ lib/nexthop.c
@@ -146,6 +146,11 @@ static int _nexthop_cmp_no_labels(const struct nexthop *next1,
 		break;
 	}
 
+	if (next1->srte_color < next2->srte_color)
+		return -1;
+	if (next1->srte_color > next2->srte_color)
+		return 1;
+
 	ret = _nexthop_source_cmp(next1, next2);
 
 	return ret;
