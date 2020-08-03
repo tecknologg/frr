9cef66bf3d8d8cc710065429bfeb58469ebadd1d
David Schweizer <dschweizer@opensourcerouting.org>
2020-08-03 08:58:23 +0200

    bgpd:   RFC 4271 DelayOpenTime config support
    * Applied fixes suggested by Rafael Zalamena:
    	- Separate "no" command.
    	- Defined timer max interval value.
    	- Usage of ALL_LIST_ELEMENTS_RO instead of ALL_LIST_ELEMENTS
    	  list iterator.
    	- Named variables in function prototypes.
    
    * For review only. Does compile, but still needs work.

--- bgpd/bgp_vty.c
+++ bgpd/bgp_vty.c
@@ -6093,7 +6093,8 @@ DEFUN (no_neighbor_timers_connect,
 
 DEFPY (neighbor_timers_delayopen,
        neighbor_timers_delayopen_cmd,
-       "[no] neighbor <A.B.C.D|X:X::X:X|WORD>$neighbor timers delayopen [(1-240)$interval]",
+       /* TODO: string concatenation with BGP_TIMERS_DELAYOPEN_MAX_INTERVAL */
+       "neighbor <A.B.C.D|X:X::X:X|WORD>$neighbor timers delayopen (1-240)$interval",
        "BGP per neighbor timers\n"
        "BGP delayopen timer\n"
        "DelayOpen timer\n")
@@ -6104,25 +6105,33 @@ DEFPY (neighbor_timers_delayopen,
 	if (!peer)
 		return CMD_WARNING_CONFIG_FAILED;
 
-	/* TODO: not entirely sure how to deal with the no stirng. */
-	/* if ( (no) || (!interval) ) { */
-	if ( (strncmp(no, (const char*)("no"), 2)) || (!interval) ) {
-		/* TODO:
-		 * unset flag, reset timer value and unset timer config for the
-		 * peer or peer group according to overwrite settings.
-		 */
+	if (!interval) {
 		if (peer_timers_delayopen_unset(peer))
 			return CMD_WARNING_CONFIG_FAILED;
 	} else {
-		/* TODO:
-		 * set flag, set timer value and set timer config for the
-		 * peer or peer group according to overwrite settings.
-		 */
 		if (peer_timers_delayopen_set(peer, interval))
 			return CMD_WARNING_CONFIG_FAILED;
 	}
 
-	/* TODO: spit out something useful to logs if necessary. */
+	return CMD_SUCCESS;
+}
+
+
+DEFPY (no_neighbor_timers_delayopen,
+       no_neighbor_timers_delayopen_cmd,
+       "no neighbor <A.B.C.D|X:X::X:X|WORD>$neighbor timers delayopen",
+       "BGP per neighbor timers\n"
+       "BGP delayopen timer\n"
+       "DelayOpen timer\n")
+{
+	struct peer *peer;
+
+	peer = peer_and_group_lookup_vty(vty, neighbor);
+	if (!peer)
+		return CMD_WARNING_CONFIG_FAILED;
+
+	if (peer_timers_delayopen_unset(peer))
+			return CMD_WARNING_CONFIG_FAILED;
 
 	return CMD_SUCCESS;
 }
@@ -16665,6 +16674,7 @@ void bgp_vty_init(void)
 
 	/* "neighbor timers delayopen" commands. */
 	install_element(BGP_NODE, &neighbor_timers_delayopen_cmd);
+	install_element(BGP_NODE, &no_neighbor_timers_delayopen_cmd);
 
 	/* "neighbor interface" commands. */
 	install_element(BGP_NODE, &neighbor_interface_cmd);
