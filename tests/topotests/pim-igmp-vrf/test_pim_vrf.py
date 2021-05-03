fb352c61108c1e7091c06d07e0bd795e4bdabe90
Martin Winter <mwinter@opensourcerouting.org>
2021-05-03 22:52:30 +0200

    tests: Add pim-igmp-vrf testsuite
    Signed-off-by: Martin Winter <mwinter@opensourcerouting.org>

--- tests/topotests/pim-igmp-vrf/test_pim_vrf.py
+++ tests/topotests/pim-igmp-vrf/test_pim_vrf.py
@@ -45,6 +45,10 @@ sys.path.append(os.path.join(CWD, "../"))
 from lib import topotest
 from lib.topogen import Topogen, TopoRouter, get_topogen
 from lib.topolog import logger
+from lib.topotest import iproute2_is_vrf_capable
+from lib.common_config import (
+    required_linux_kernel_version,
+    adjust_router_l3mdev )
 
 # Required to instantiate the topology builder class.
 from mininet.topo import Topo
@@ -116,25 +120,22 @@ class PIMVRFTopo(Topo):
         tgen = get_topogen(self)
 
         # Create the hosts
-        for hostNum in range(1,3):
+        for hostNum in range(1,5):
             tgen.add_router("h{}".format(hostNum))
 
-        # Create the main router - without VRF
+        # Create the main router
         tgen.add_router("r1")
-        # Create the 2nd main router - with VRF
-        tgen.add_router("r2")
 
         # Create the PIM RP routers
-        for rtrNum in range(11, 16):
+        for rtrNum in range(11, 13):
             tgen.add_router("r{}".format(rtrNum))
 
         # Setup Switches and connections
         for swNum in range(1, 5):
             tgen.add_switch("sw{}".format(swNum))
-
         
         ################
-        # 1st set of connections to routers without VRF
+        # 1st set of connections to routers for VRF red
         ################
 
         # Add connections H1 to R1 switch sw1
@@ -145,28 +146,19 @@ class PIMVRFTopo(Topo):
         tgen.gears["r1"].add_link(tgen.gears["sw2"])
         tgen.gears["h2"].add_link(tgen.gears["sw2"])
         tgen.gears["r11"].add_link(tgen.gears["sw2"])
-        tgen.gears["r12"].add_link(tgen.gears["sw2"])
-        tgen.gears["r13"].add_link(tgen.gears["sw2"])
-        tgen.gears["r14"].add_link(tgen.gears["sw2"])
-        tgen.gears["r15"].add_link(tgen.gears["sw2"])
 
         ################
-        # 2nd set of connections to routers without VRF
+        # 2nd set of connections to routers for vrf blue
         ################
 
         # Add connections H1 to R1 switch sw1
-        tgen.gears["h1"].add_link(tgen.gears["sw3"])
+        tgen.gears["h3"].add_link(tgen.gears["sw3"])
         tgen.gears["r1"].add_link(tgen.gears["sw3"])
 
         # Add connections R1 to R1x switch sw2
         tgen.gears["r1"].add_link(tgen.gears["sw4"])
-        tgen.gears["h2"].add_link(tgen.gears["sw4"])
-        tgen.gears["r11"].add_link(tgen.gears["sw4"])
+        tgen.gears["h4"].add_link(tgen.gears["sw4"])
         tgen.gears["r12"].add_link(tgen.gears["sw4"])
-        tgen.gears["r13"].add_link(tgen.gears["sw4"])
-        tgen.gears["r14"].add_link(tgen.gears["sw4"])
-        tgen.gears["r15"].add_link(tgen.gears["sw4"])
-
 
 #####################################################
 #
@@ -179,20 +171,24 @@ def setup_module(module):
     tgen.start_topology()
 
     vrf_setup_cmds = [
-        "ip link add {0}-cust1 type vrf table 1001",
-        "ip link add loop1 type dummy",
-        "ip link set {0}-eth0 master {0}-cust1",
-        "ip link set {0}-eth1 master {0}-cust1",
+        "ip link add name blue type vrf table 11",
+        "ip link add name red type vrf table 12",
+        "ip link set dev blue up",
+        "ip link set dev red up",
+        "ip link set dev r1-eth0 vrf blue up",
+        "ip link set dev r1-eth1 vrf blue up",
+        "ip link set dev r1-eth2 vrf red up",
+        "ip link set dev r1-eth3 vrf red up",
     ]
 
     # Starting Routers
     router_list = tgen.routers()
 
     # Create VRF on r2 first and add it's interfaces
-    for cmd in cmds:
-        tgen.net["r2"].cmd(cmd.format(rname))
+    for cmd in vrf_setup_cmds:
+        tgen.net["r1"].cmd(cmd)
     # adjust handling of vrf traffic
-    adjust_router_l3mdev(tgen, "r2")
+    adjust_router_l3mdev(tgen, "r1")
 
     for rname, router in router_list.items():
         logger.info("Loading router %s" % rname)
@@ -220,21 +216,43 @@ def test_ospf_convergence():
     "Test for OSPFv2 convergence"
     tgen = get_topogen()
 
+    # Required linux kernel version for this suite to run.
+    result = required_linux_kernel_version("4.15")
+    if result is not True:
+        pytest.skip("Kernel requirements are not met")
+
+    # iproute2 needs to support VRFs for this suite to run.
+    if not iproute2_is_vrf_capable():
+        pytest.skip("Installed iproute2 version does not support VRFs")
+
     # Skip if previous fatal error condition is raised
     if tgen.routers_have_failure():
         pytest.skip(tgen.errors)
 
-    logger.info("Checking OSPFv2 convergence on router r1")
+    logger.info("Checking OSPFv2 convergence on router r1 for VRF blue")
 
     router = tgen.gears["r1"]
-    reffile = os.path.join(CWD, "r1/ospf_neighbor.json")
+    reffile = os.path.join(CWD, "r1/ospf_blue_neighbor.json")
     expected = json.loads(open(reffile).read())
 
     test_func = functools.partial(
-        topotest.router_json_cmp, router, "show ip ospf neighbor json", expected
+        topotest.router_json_cmp, router, "show ip ospf vrf blue neighbor json", expected
     )
     _, res = topotest.run_and_expect(test_func, None, count=60, wait=2)
-    assertmsg = "OSPF router R1 did not converge"
+    assertmsg = "OSPF router R1 did not converge on VRF blue"
+    assert res is None, assertmsg
+
+    logger.info("Checking OSPFv2 convergence on router r1 for VRF red")
+
+    router = tgen.gears["r1"]
+    reffile = os.path.join(CWD, "r1/ospf_red_neighbor.json")
+    expected = json.loads(open(reffile).read())
+
+    test_func = functools.partial(
+        topotest.router_json_cmp, router, "show ip ospf vrf red neighbor json", expected
+    )
+    _, res = topotest.run_and_expect(test_func, None, count=60, wait=2)
+    assertmsg = "OSPF router R1 did not converge on VRF red"
     assert res is None, assertmsg
 
 
@@ -246,162 +264,121 @@ def test_pim_convergence():
     if tgen.routers_have_failure():
         pytest.skip(tgen.errors)
 
-    logger.info("Checking PIM convergence on router r1")
-
-    tgen.mininet_cli()
+    logger.info("Checking PIM convergence on router r1 for VRF red")
 
     router = tgen.gears["r1"]
-    reffile = os.path.join(CWD, "r1/pim_neighbor.json")
+    reffile = os.path.join(CWD, "r1/pim_red_neighbor.json")
     expected = json.loads(open(reffile).read())
 
     test_func = functools.partial(
-        topotest.router_json_cmp, router, "show ip pim neighbor json", expected
+        topotest.router_json_cmp, router, "show ip pim vrf red neighbor json", expected
     )
     _, res = topotest.run_and_expect(test_func, None, count=60, wait=2)
-    assertmsg = "PIM router R1 did not converge"
+    assertmsg = "PIM router R1 did not converge for VRF red"
+    assert res is None, assertmsg
+
+    logger.info("Checking PIM convergence on router r1 for VRF blue")
+
+    router = tgen.gears["r1"]
+    reffile = os.path.join(CWD, "r1/pim_blue_neighbor.json")
+    expected = json.loads(open(reffile).read())
+
+    test_func = functools.partial(
+        topotest.router_json_cmp, router, "show ip pim vrf blue neighbor json", expected
+    )
+    _, res = topotest.run_and_expect(test_func, None, count=60, wait=2)
+    assertmsg = "PIM router R1 did not converge for VRF blue"
     assert res is None, assertmsg
 
 
-# def test_dummy():
-#     "Dummy Test with CLI"
-#     tgen = get_topogen()
+def test_dummy():
+    "Dummy Test with CLI"
+    tgen = get_topogen()
 
-#     # Skip if previous fatal error condition is raised
-#     if tgen.routers_have_failure():
-#         pytest.skip(tgen.errors)
+    # Skip if previous fatal error condition is raised
+    if tgen.routers_have_failure():
+        pytest.skip(tgen.errors)
 
-#     logger.info("Dummy Test started");
-#     tgen.mininet_cli()
+    logger.info("Dummy Test started");
+    tgen.mininet_cli()
 
-#     tgen.gears["h2"].run("{} --send='0.7' '{}' '{}' '{}' &".format(
-#         HELPER_APP_PATH, APP_SOCK_PATH, '239.100.0.17', 'h2-eth0'))
-#     accept_host("h2")
+    # tgen.gears["h2"].run("{} --send='0.7' '{}' '{}' '{}' &".format(
+    #     HELPER_APP_PATH, APP_SOCK_PATH, '239.100.0.17', 'h2-eth0'))
+    # accept_host("h2")
 
-#     tgen.gears["h1"].run("{} '{}' '{}' '{}' &".format(
-#         HELPER_APP_PATH, APP_SOCK_PATH, '239.100.0.17', 'h1-eth0'))
-#     accept_host("h1")
+    # tgen.gears["h1"].run("{} '{}' '{}' '{}' &".format(
+    #     HELPER_APP_PATH, APP_SOCK_PATH, '239.100.0.17', 'h1-eth0'))
+    # accept_host("h1")
 
-#     logger.info("join issued")
-#     tgen.mininet_cli()
+    # logger.info("join issued")
+    # tgen.mininet_cli()
 
 
 ##################################
 ###  Test PIM RP Access-Lists
 ##################################
 
-def check_mcast_entry(entry, mcastaddr, pimrp):
+def check_mcast_entry(mcastaddr, pimrp, receiver, sender, vrf):
     "Helper function to check RP"
     tgen = get_topogen()
 
-    logger.info("Testing PIM RP selection for ACL {} entry using {}".format(entry, mcastaddr));
+    logger.info("Testing PIM for VRF {} entry using {}".format(vrf, mcastaddr));
 
     # Start applications socket.
     listen_to_applications()
 
-    tgen.gears["h2"].run("{} --send='0.7' '{}' '{}' '{}' &".format(
-        HELPER_APP_PATH, APP_SOCK_PATH, mcastaddr, 'h2-eth0'))
-    accept_host("h2")
+    tgen.gears[sender].run("{} --send='0.7' '{}' '{}' '{}' &".format(
+        HELPER_APP_PATH, APP_SOCK_PATH, mcastaddr, '{}-eth0'.format(sender)))
+    accept_host(sender)
 
-    tgen.gears["h1"].run("{} '{}' '{}' '{}' &".format(
-        HELPER_APP_PATH, APP_SOCK_PATH, mcastaddr, 'h1-eth0'))
-    accept_host("h1")
+    tgen.gears[receiver].run("{} '{}' '{}' '{}' &".format(
+        HELPER_APP_PATH, APP_SOCK_PATH, mcastaddr, '{}-eth0'.format(receiver)))
+    accept_host(receiver)
 
     logger.info("mcast join and source for {} started".format(mcastaddr))
 
-    # tgen.mininet_cli()
+    tgen.mininet_cli()
 
     router = tgen.gears["r1"]
-    reffile = os.path.join(CWD, "r1/acl_{}_pim_join.json".format(entry))
+    reffile = os.path.join(CWD, "r1/pim_{}_join.json".format(vrf))
     expected = json.loads(open(reffile).read())
 
-    logger.info("verifying pim join on r1 for {}".format(mcastaddr))
+    logger.info("verifying pim join on r1 for {} on VRF {}".format(mcastaddr, vrf))
     test_func = functools.partial(
-        topotest.router_json_cmp, router, "show ip pim join json", expected
+        topotest.router_json_cmp, router, "show ip pim vrf {} join json".format(vrf),
+        expected
     )
     _, res = topotest.run_and_expect(test_func, None, count=60, wait=2)
-    assertmsg = "PIM router r1 did not show join status"
+    assertmsg = "PIM router r1 did not show join status on VRF".format(vrf)
     assert res is None, assertmsg
 
     logger.info("verifying pim join on PIM RP {} for {}".format(pimrp, mcastaddr))
     router = tgen.gears[pimrp]
-    reffile = os.path.join(CWD, "{}/acl_{}_pim_join.json".format(pimrp, entry))
+    reffile = os.path.join(CWD, "{}/pim_{}_join.json".format(pimrp, vrf))
     expected = json.loads(open(reffile).read())
 
     test_func = functools.partial(
         topotest.router_json_cmp, router, "show ip pim join json", expected
     )
     _, res = topotest.run_and_expect(test_func, None, count=60, wait=2)
-    assertmsg = "PIM router {} did not get selected as the PIM RP".format(pimrp)
+    assertmsg = "PIM router {} did not get selected as the PIM RP for VRF {}".format(pimrp, vrf)
     assert res is None, assertmsg
 
     close_applications()
     return
 
 
-def test_mcast_acl_1():
-    "Test 1st ACL entry 239.100.0.0/28 with 239.100.0.1"
+def test_mcast_vrf_red():
+    "Test vrf red with 239.100.0.17"
     tgen = get_topogen()
 
     # Skip if previous fatal error condition is raised
     if tgen.routers_have_failure():
         pytest.skip(tgen.errors)
 
-    check_mcast_entry(1, '239.100.0.1', 'r11')
+    check_mcast_entry('239.100.0.1', 'r11', 'h1', 'h2', 'red')
 
 
-def test_mcast_acl_2():
-    "Test 2nd ACL entry 239.100.0.17/32 with 239.100.0.17"
-    tgen = get_topogen()
-
-    # Skip if previous fatal error condition is raised
-    if tgen.routers_have_failure():
-        pytest.skip(tgen.errors)
-
-    check_mcast_entry(2, '239.100.0.17', 'r12')
-
-
-def test_mcast_acl_3():
-    "Test 3rd ACL entry 239.100.0.32/27 with 239.100.0.32"
-    tgen = get_topogen()
-
-    # Skip if previous fatal error condition is raised
-    if tgen.routers_have_failure():
-        pytest.skip(tgen.errors)
-
-    check_mcast_entry(3, '239.100.0.32', 'r13')
-
-
-def test_mcast_acl_4():
-    "Test 4th ACL entry 239.100.0.128/25 with 239.100.0.255"
-    tgen = get_topogen()
-
-    # Skip if previous fatal error condition is raised
-    if tgen.routers_have_failure():
-        pytest.skip(tgen.errors)
-
-    check_mcast_entry(4, '239.100.0.255', 'r14')
-
-
-def test_mcast_acl_5():
-    "Test 5th ACL entry 239.100.0.96/28 with 239.100.0.97"
-    tgen = get_topogen()
-
-    # Skip if previous fatal error condition is raised
-    if tgen.routers_have_failure():
-        pytest.skip(tgen.errors)
-
-    check_mcast_entry(5, '239.100.0.97', 'r14')
-
-
-def test_mcast_acl_6():
-    "Test 5th ACL entry 239.100.0.64/28 with 239.100.0.70"
-    tgen = get_topogen()
-
-    # Skip if previous fatal error condition is raised
-    if tgen.routers_have_failure():
-        pytest.skip(tgen.errors)
-
-    check_mcast_entry(6, '239.100.0.70', 'r15')
 
 
 
