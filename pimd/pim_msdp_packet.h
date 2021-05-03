f62f7b528d5478baa5ca014712e33988de9313ca
Rafael Zalamena <rzalamena@opensourcerouting.org>
2021-05-03 10:25:52 -0300

    pimd: MSDP SA filtering
    Implement MSDP peer incoming/outgoing SA filter.
    
    Note
    ----
    
    Cisco extended access list has a special meaning: the second address is
    the source address to filter.
    
    Example:
    
      access-list filter-lan-group deny ip 224.0.0.0 0.255.255.255 192.168.0.0 0.0.255.255
      access-list filter-lan-group permit any
      ip msdp peer 192.168.0.1 sa-filter filter-lan-group out
    
    Signed-off-by: Rafael Zalamena <rzalamena@opensourcerouting.org>

--- pimd/pim_msdp_packet.h
+++ pimd/pim_msdp_packet.h
@@ -69,5 +69,7 @@ void pim_msdp_pkt_sa_tx_one(struct pim_msdp_sa *sa);
 void pim_msdp_pkt_sa_tx_to_one_peer(struct pim_msdp_peer *mp);
 void pim_msdp_pkt_sa_tx_one_to_one_peer(struct pim_msdp_peer *mp,
 					struct in_addr rp, struct prefix_sg sg);
+bool msdp_peer_sa_filter(struct pim_msdp_peer *mp,
+			 const struct pim_msdp_sa *sa);
 
 #endif
